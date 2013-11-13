8e9d38c8b2164a419816c00d8f7366ca0d857810
Terry.Mao <83873308@qq.com>
2013-11-13 15:59:18 +0800

    add memory stat

--- stat.go
+++ stat.go
@@ -96,9 +96,38 @@ func GetMemStats() []byte {
 	//TODO
 	m := &runtime.MemStats{}
 	runtime.ReadMemStats(m)
-
+	// general
 	res := map[string]interface{}{}
-	res["todo"] = 1
+	res["alloc"] = m.Alloc
+	res["total_alloc"] = m.TotalAlloc
+	res["sys"] = m.Sys
+	res["lookups"] = m.Lookups
+	res["mallocs"] = m.Mallocs
+	res["frees"] = m.Frees
+	// heap
+	res["heap_alloc"] = m.HeapAlloc
+	res["heap_sys"] = m.HeapSys
+	res["heap_idle"] = m.HeapIdle
+	res["heap_inuse"] = m.HeapInuse
+	res["heap_released"] = m.HeapReleased
+	res["heap_objects"] = m.HeapObjects
+	// low-level fixed-size struct alloctor
+	res["stack_inuse"] = m.StackInuse
+	res["stack_sys"] = m.StackSys
+	res["mspan_inuse"] = m.MSpanInuse
+	res["mspan_sys"] = m.MSpanSys
+	res["mcache_inuse"] = m.MCacheInuse
+	res["mcache_sys"] = m.MCacheSys
+	res["buckhash_sys"] = m.BuckHashSys
+	// GC
+	res["next_gc"] = m.NextGC
+	res["last_gc"] = m.LastGC
+	res["pause_total_ns"] = m.PauseTotalNs
+	res["pause_ns"] = m.PauseNs
+	res["num_gc"] = m.NumGC
+	res["enable_gc"] = m.EnableGC
+	res["debug_gc"] = m.DebugGC
+	res["by_size"] = m.BySize
 
 	return jsonRes(res)
 }
