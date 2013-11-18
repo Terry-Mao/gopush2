9f6ce6bf8116c1b3aa2d171c9b3b07e4d6827f7c
Terry.Mao <83873308@qq.com>
2013-11-18 17:46:04 +0800

    split channel inner map into bucket use a mmhash function

--- channel.go
+++ channel.go
@@ -1,38 +1,63 @@
 package main
 
 import (
+	"github.com/Terry-Mao/gopush2/mmhash"
 	"sync"
 	"time"
 )
 
-type Channel struct {
-	did   map[string]*Subscriber
+const (
+	bucketSize = 16
+)
+
+type channelBucket struct {
+	data  map[string]*Subscriber
 	mutex *sync.Mutex
+}
+
+type Channel struct {
+	subscriber []*channelBucket
 }
 
 func NewChannel() *Channel {
 	c := &Channel{}
-	c.did = map[string]*Subscriber{}
-	c.mutex = &sync.Mutex{}
+	c.subscriber = []*channelBucket{}
+	// split hashmap to 16 bucket
+	for i := 0; i < bucketSize; i++ {
+		b := &channelBucket{
+			data:  map[string]*Subscriber{},
+			mutex: &sync.Mutex{},
+		}
+
+		c.subscriber = append(c.subscriber, b)
+	}
 
 	return c
 }
 
+// get a bucket from channel
+func (c *Channel) bucket(key string) *channelBucket {
+	idx := mmhash.MurMurHash2(key) & (bucketSize - 1)
+	return c.subscriber[idx]
+}
+
 // get a subscriber from channel in pub/sub action
-func (c *Channel) GetSubscriber(key string) *Subscriber {
+func (c *Channel) Subscriber(key string) *Subscriber {
 	var (
 		s   *Subscriber
 		ok  bool
 		now = time.Now().UnixNano()
 	)
 
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	// get a channel bucket
+	b := c.bucket(key)
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
 
-	if s, ok = c.did[key]; !ok {
+	if s, ok = b.data[key]; !ok {
 		// not exists subscriber for the key
 		s = NewSubscriber(key)
-		c.did[key] = s
+		b.data[key] = s
 		channelStats.IncrCreated()
 	} else {
 		// check expired
@@ -42,7 +67,7 @@ func (c *Channel) GetSubscriber(key string) *Subscriber {
 			Log.Printf("device %s drop the expired channel, refresh a new one now(%d) > expire(%d)", key, now, s.Expire)
 			s.CloseAllConn()
 			s = NewSubscriber(key)
-			c.did[key] = s
+			b.data[key] = s
 			channelStats.IncrExpired()
 		} else {
 			// refresh the expire time
